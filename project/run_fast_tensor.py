81673d884fa80665934912c823d291129b8e26c1
Mihika <mj434@cornell.edu>
2024-11-19 00:55:48 -0500

    implementing run tensor

--- project/run_fast_tensor.py
+++ project/run_fast_tensor.py
@@ -29,8 +29,9 @@ class Network(minitorch.Module):
         self.layer3 = Linear(hidden, 1, backend)
 
     def forward(self, x):
-        # TODO: Implement for Task 3.5.
-        raise NotImplementedError("Need to implement for Task 3.5")
+        one = self.layer1.forward(x).relu()
+        two = self.layer2.forward(one).relu()
+        return self.layer3.forward(two).sigmoid()
 
 
 class Linear(minitorch.Module):
@@ -43,8 +44,12 @@ class Linear(minitorch.Module):
         self.out_size = out_size
 
     def forward(self, x):
-        # TODO: Implement for Task 3.5.
-        raise NotImplementedError("Need to implement for Task 3.5")
+        size, feats = x.shape
+        output_dim = self.weights.value.shape[1]
+        reshaped_x = x.view(size, feats, 1)
+        reshaped_weights = self.weights.value.view(1, feats, output_dim)
+        bias_term = self.bias.value
+        return (reshaped_x * reshaped_weights).sum(dim=1).view(size, output_dim) + bias_term
 
 
 class FastTrain:
